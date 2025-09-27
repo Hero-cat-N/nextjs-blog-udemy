b945ed3d6ac4b760438e347e9a5f81bd7dbefc78
Hero_cat_N <hirokazu555913@gmail.com>
2025-09-27 14:48:43 +0900

    22. _app.jsにグローバルスタイリングを適用させてみよう

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import styles from "./layout.module.css"
 // note: style.containerとする場合はcssはhogehoge.module.cssとする必要がある
 
+import utilStyles from "../styles/utils.module.css"
+
 const name = "Hero-cat's Code";
 export const siteTitle = "Next.js blog";
 
@@ -15,8 +17,8 @@ function Layout( {children} ) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" width={160} />
-          <h1>{name}</h1>
+        <img src="/images/profile.png" width={160} className={utilStyles.borderCircle} />
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>
         {children}
