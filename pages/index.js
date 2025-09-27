f8dacc341aaee3ba38f64a73fd3aaa8cd02537a9
Hero_cat_N <hirokazu555913@gmail.com>
2025-09-27 15:12:15 +0900

    23. トップページのレイアウトを更新しよう
    23. トップページのレイアウトを更新しよう

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,30 @@ import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 
+import utilStyle from "../styles/utils.module.css";
+
 export default function Home() {
   return (
     <Layout>
-      <a href="">Next.jsです</a>
+      <section className={utilStyle.headingMd}>
+        <p>私はフロントエンドエンジニアを目指しています</p>
+      </section>
+
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" />
+            </Link>
+            <Link href="/">
+              SSGとSSRの使い分けの部分はいつなのか？
+            </Link>
+            <br />
+            <small>September, 27, 2025</small>
+          </article>
+        </div>
+      </section>
     </Layout>
   );
 }
