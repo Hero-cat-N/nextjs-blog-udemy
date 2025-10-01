e3b59dda8dd6e66c1865529833164672a0a8fe6e
Hero_cat_N <hirokazu555913@gmail.com>
2025-10-01 18:34:34 +0900

    33. Next.jsにおける任意のURLを動的ルーティングとして設定する方法

---
+++ pages/posts/[id].js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+// note: [id].jsとして任意のURLをつけることができる
+
+export default function Post() {
+  return (
+    <Layout>
+      動的ルーティング設定
+    </Layout>
+  );
+}
