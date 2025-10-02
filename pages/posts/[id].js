d486f5f52c8d8dc49ae6af89f040c499aabc319f
Hero_cat_N <hirokazu555913@gmail.com>
2025-10-02 18:50:17 +0900

    35. getStaticPathsを実際に使ってみよう

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,14 @@
 import Layout from "@/components/Layout";
+import { getAllPostIds } from "@/lib/post";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 // note: [id].jsとして任意のURLをつけることができる
 
